0865cb5f4f872e1e8fbc964170d07794abd8bb27
SRAVANKUMARBUNNY <ipplisravan533@gmail.com>
2025-07-28 12:12:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import react from '@vitejs/plugin-react'
+function App() {
+  return (
+    <h1 style={{ marginTop: '20px', textAlign: 'left', paddingLeft: '20px' }}>
+      Welcome to 3rd year CICD S227
+    </h1>
+  )
+}
+
+export default App
